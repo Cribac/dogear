2afcf225cb7cd33138b2f9f17918775b7ef804ed
Christian Bachhelm <cbachhelm@tuta.io>
2024-06-03 10:19:09 +0200

    Establish Supabase client
    Closes #3

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
+  readonly PUBLIC_SUPABASE_URL: string
+  readonly PUBLIC_SUPABASE_KEY: string
 }
 
 interface ImportMeta {
