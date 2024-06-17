17fb2a9356002285cac441ea2d8831b0c2ef13ff
Christian Bachhelm <cbachhelm@tuta.io>
2024-06-17 10:11:38 +0200

    Setup vee-validate for signup form

--- src/components/forms/SigninForm.vue
+++ src/components/forms/SigninForm.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useForm } from 'vee-validate'
+import { object, string } from 'yup'
+import { toTypedSchema } from '@vee-validate/yup'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
-import { ref } from 'vue'
 
 const errorMessage = ref<string | null>(null)
 
-async function onSubmit(e: Event) {
-  e.preventDefault()
-  const formData = new FormData(e.currentTarget as HTMLFormElement)
+const { errors, handleSubmit, defineField } = useForm({
+  validationSchema: object({
+    email: string().required().email(),
+    password: string().min(0).required(),
+  }),
+})
+
+const [email, emailAttrs] = defineField('email')
+const [password, passwordAttrs] = defineField('password')
+
+const onSubmit = handleSubmit(async (values) => {
+  console.log(JSON.stringify(values))
+
+  const formData = new FormData()
+  formData.append('email', values.email)
+  formData.append('password', values.password)
+
   const response = await fetch('/api/auth/signin', {
     method: 'POST',
     body: formData,
@@ -19,20 +36,19 @@ async function onSubmit(e: Event) {
     errorMessage.value = null
     window.location.href = '/'
   }
-}
+})
+
 </script>
 
 <template>
   <div>
     <form @submit="onSubmit">
-      <Label for="email">
-        Email
-      </Label>
-      <Input id="email" type="email" name="email" placeholder="me@example.com" />
-      <Label for="password">
-        Password
-      </Label>
-      <Input id="password" type="password" name="password" />
+      <Label for="email"> Email </Label>
+      <Input id="email" type="email" name="email" v-model="email" v-bind="emailAttrs" />
+      <div>{{ errors.email }}</div>
+      <Label for="password"> Password </Label>
+      <Input id="password" type="password" name="password" v-model="password" v-bind="passwordAttrs" />
+      <div>{{ errors.password }}</div>
       <button type="submit">Login</button>
       <div v-if="errorMessage">
         {{ errorMessage }}
