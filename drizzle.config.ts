60edadf7f1404adc02c3d46488fff21ab46f662a
Christian Bachhelm <cbachhelm@tuta.io>
2024-06-23 20:53:09 +0200

    Add drizzle configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'drizzle-kit'
+import { DB_URL } from '@/config'
+
+if (!DB_URL) {
+  console.error('🔴 Cannot find database url')
+}
+
+export default defineConfig({
+  schema: './src/lib/db/schema.ts',
+  out: './migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: DB_URL || ''
+  },
+  verbose: true,
+  strict: true,
+})
