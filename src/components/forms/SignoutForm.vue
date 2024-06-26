ecfee7474afde3e11c6fc326e5289fbd87cae176
Christian Bachhelm <cbachhelm@tuta.io>
2024-06-26 11:27:47 +0200

    Add SignoutForm component

---
+++ src/components/forms/SignoutForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <form action="/api/auth/signout">
+    <Button type="submit">Sign out</Button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+</script>
