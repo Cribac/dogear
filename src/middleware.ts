62e7a650d699172ce7a2d22dd591e4bda8cda58e
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-04 20:31:17 +0200

    Add middleware for some authorization

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { defineMiddleware } from 'astro:middleware'
+import { supabase } from '@/lib/supabase'
+
+export const onRequest = defineMiddleware(async (context , next) => {
+  const response = await next()
+  const { pathname } = context.url
+  const allowedRoutes = [
+    '/', 
+    '/signin',
+    '/register',
+    '/api/auth/signin',
+    '/api/auth/signout',
+    '/api/auth/register',
+  ]
+
+  if (!allowedRoutes.includes(pathname)) {
+    let token = null
+    const requestHeaders = context.request.headers.get('Authorization')
+  
+    if (requestHeaders) {
+      token = requestHeaders.replace('Bearer', '').trim()
+    }
+  
+    const { data: { session } } = await supabase.auth.getSession()
+
+    if (token && session?.access_token !== undefined && (token === session?.access_token)) {
+      return response
+    } else {
+      return new Response(
+        JSON.stringify({
+          message: 'Unauthorized',
+        }),
+        { status: 401 },
+      )
+    }
+  }
+
+  return response
+})
