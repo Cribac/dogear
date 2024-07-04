d2b24370505ea2644c9e3e47029794705ffcad0f
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-04 20:36:06 +0200

    Ensure session is properly terminated after signout

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,7 +1,10 @@
 import type { APIRoute } from 'astro'
+import { supabase } from '@/lib/supabase'
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
   cookies.delete('sb-access-token', { path: '/' })
   cookies.delete('sb-refresh-token', { path: '/' })
+
+  supabase.auth.signOut()
   return redirect('/signin')
 }
