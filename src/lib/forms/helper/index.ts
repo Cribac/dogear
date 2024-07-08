02b7f008bf38fe9328847e5997078a76fe582f51
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-08 17:20:00 +0200

    Account for the profileId when building form data

--- src/lib/forms/helper/index.ts
+++ src/lib/forms/helper/index.ts
@@ -2,13 +2,19 @@
  * Builds a FormData object from an object
  *
  * @param values {[key: string]: string}
+ * @param {?string} profileId
  * @returns {FormData}
  */
-function buildFormData(values: Record<string, string>): FormData {
+function buildFormData(values: Record<string, string>, profileId?: string): FormData {
   const formData = new FormData()
   for (const [key, value] of Object.entries(values)) {
     formData.append(key, value)
   }
+
+  if (profileId) {
+    formData.append('profileId', profileId)
+  }
+
   return formData
 }
 
