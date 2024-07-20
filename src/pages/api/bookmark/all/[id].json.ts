57a161f94094546c2656446e68dfdde9f0520ce6
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-20 09:46:52 +0200

    Add category_name in the bookmark query

--- src/pages/api/bookmark/all/[id].json.ts
+++ src/pages/api/bookmark/all/[id].json.ts
@@ -2,15 +2,25 @@ import type { APIRoute } from 'astro'
 import { eq } from 'drizzle-orm'
 import { db } from '@/lib/db/db'
 import { Bookmark } from '@/lib/db/schemas'
+import { Category } from '@/lib/db/schemas'
 
 export const GET: APIRoute = async (context) => {
   // @TODO harden this!
   const { id } = context.params
 
   const bookmarks = await db
-    .select()
+    .select({
+      id: Bookmark.id,
+      name: Bookmark.name,
+      url: Bookmark.url,
+      created_at: Bookmark.created_at,
+      updated_at: Bookmark.updated_at,
+      category_id: Bookmark.categoryId,
+      category_name: Category.name
+    })
     .from(Bookmark)
     .where(eq(Bookmark.profileId, (id ? id : 'none'))) // jeeebus creebus hacky whacky
+    .innerJoin(Category, eq(Bookmark.categoryId, Category.id))
     .orderBy(Bookmark.created_at)
 
   return new Response(
