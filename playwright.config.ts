2365c471623c93ef42ce0c83ee3a153d3ece8418
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-21 17:20:59 +0200

    Run test server via pnpm

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: 'pnpm run dev',
     url: 'http://localhost:4321/',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
