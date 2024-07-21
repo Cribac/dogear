683ed23d71c0ce2b4b74b12e1f9bb0c38f6bfa15
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-21 17:22:06 +0200

    Add e2e spec for register page

---
+++ e2e/register.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect } from '@playwright/test'
+
+test('register page shows register form', async ({ page }) => {
+  await page.goto('/register')
+  
+  await expect(page.getByRole('heading', { name: 'Register Account' })).toBeVisible()
+  await expect(page.getByLabel('Email')).toBeVisible()
+  await expect(page.getByLabel('Password')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Register' })).toBeVisible()
+})
+
+test('it displays a message when email is invalid', async ({ page }) => {
+  await page.goto('/register')
+  
+  await page.getByLabel('Email').click()
+  await page.waitForTimeout(1000) // @TODO: needed rn to work in chromium; investigate why
+  await page.getByLabel('Email').fill('invalid')
+  await page.getByRole('button', { name: 'Register' }).click()
+
+  await expect(page.getByText('email must be a valid email')).toBeVisible()
+})
+
+test('it displays a message when no email is provided', async ({ page }) => {
+  await page.goto('/register')
+
+  await page.waitForTimeout(1000)
+  await page.getByRole('button', { name: 'Register' }).click()
+  
+  await expect(page.getByText('email is a required field')).toBeVisible()
+})
+
+test('it displays a message when no password is provided', async ({ page }) => {
+  await page.goto('/register')
+  
+  await page.getByLabel('Email').click()
+  await page.waitForTimeout(1000) // @TODO: needed rn to work in chromium; investigate why
+  await page.getByLabel('Email').fill('foo@example.com')
+  await page.getByRole('button', { name: 'Register' }).click()
+  
+  await expect(page.getByText('password is a required field')).toBeVisible()
+})
