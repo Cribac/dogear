f796582fdb50efa83b1ba3f6c331af3ef648864c
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-22 13:11:56 +0200

    Establish dedicated components for pagination-controls

---
+++ src/components/ui/data-table/pagination-controls/index.ts
@@ -0,0 +1,3 @@
+export { default as ControlsContainer } from './ControlsContainer.vue'
+export { default as SelectionIndicator } from './SelectionIndicator.vue'
+export { default as PaginationButtons } from './PaginationButtons.vue'
