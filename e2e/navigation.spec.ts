cbf6d352e1c182d603ff994f5a81c11e405b694e
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-22 09:44:31 +0200

    Provide e2e test for navigation

---
+++ e2e/navigation.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test'
+
+test('a logged in user can navigate to categories', async ({ page }) => {
+  await page.goto('/')
+  
+  await expect(page.getByRole('link', { name: 'Categories' })).toBeVisible()
+  await page.getByRole('link', { name: 'Categories' }).click()
+  
+  await expect(page).toHaveURL('/categories')
+})
+
+test('a click on the logo navigates to the homepage', async ({ page }) => {
+  await page.goto('/')
+  
+  await page.getByRole('link', { name: 'Categories' }).click()
+  await expect(page).toHaveURL('/categories')
+
+  await page.getByTestId('logo').click()
+  await expect(page).toHaveURL('/')
+})
