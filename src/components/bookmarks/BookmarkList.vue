78ce3831f09d24c1093ff1d943bb898022457c9d
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-29 11:11:19 +0200

    Enable deletion of selected bookmarks

--- src/components/bookmarks/BookmarkList.vue
+++ src/components/bookmarks/BookmarkList.vue
@@ -5,6 +5,7 @@ import { columns } from '@/components/bookmarks/columns'
 import { Input } from '@/components/ui/input'
 import { DataTable } from '@/components/ui/data-table'
 import EditBookmark from '@/components/forms/EditBookmark.vue'
+import DeleteButton from '@/components/ui/DeleteButton.vue'
 
 const props = defineProps({
   userId: {
@@ -42,6 +43,19 @@ async function deleteBookmark (id: string) {
   return result
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function handleBulkDelete (rows: any[]) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const ids = rows.map((row: { original: { id: any } }) => row.original.id)
+  const url = `${props.site}/api/bookmark/delete/deleteAll.json`
+
+  const response = await fetchResponse(url, 'DELETE', PUBLIC_APP_API_TOKEN, JSON.stringify(ids))
+
+  const result = await response.json()
+  bookmarkList.value = await fetchBookmarks(props.userId, PUBLIC_APP_API_TOKEN)
+  return result
+}
+
 onMounted(async () => {
   bookmarkList.value = await fetchBookmarks(props.userId, PUBLIC_APP_API_TOKEN)
 
@@ -62,8 +76,8 @@ onMounted(async () => {
     bookmarkList.value = await fetchBookmarks(props.userId, PUBLIC_APP_API_TOKEN)
   })
 
-    // @TODO harden this
-    // @TODO: there has to be a better way than this...
+  // @TODO harden this
+  // @TODO: there has to be a better way than this...
   // @ts-expect-error don't want to type events for now
   window.addEventListener('BookmarkEdited', async (e: CustomEvent) => {
     console.log('BookmarkEdited', e.detail)
@@ -79,7 +93,7 @@ onMounted(async () => {
       :data="bookmarkList" 
     >
       <template #filters="filtersProps">
-        <div class="flex items-center py-4">
+        <div class="flex flex-1 items-center justify-between py-4">
           <div class="flex items-center py-4">
             <Input
               :model-value="filtersProps.table.getColumn('name')?.getFilterValue() as string"
@@ -89,12 +103,22 @@ onMounted(async () => {
             />
           </div>
 
-          <div class="flex items-center py-4 ml-4">
+          <div class="flex flex-1 items-center py-4 ml-4">
             <Input
               :model-value="filtersProps.table.getColumn('category_name')?.getFilterValue() as string"
               class="max-w-sm"
               placeholder="Filter by category..."
               @update:model-value="filtersProps.table.getColumn('category_name')?.setFilterValue($event)" 
+            />
+          </div>
+
+          <div
+            v-if="filtersProps.table.getSelectedRowModel().rows.length > 0"
+            class="flex justify-end items-center py-4 mr-4" 
+          >
+            <DeleteButton
+              button-text="Delete Selected"
+              @delete="handleBulkDelete(filtersProps.table.getSelectedRowModel().rows)"
             />
           </div>
         </div>
---
+++ src/pages/api/bookmark/delete/deleteAll.json.ts
@@ -0,0 +1,24 @@
+import type { APIRoute } from 'astro'
+import { eq } from 'drizzle-orm'
+import { db } from '@/lib/db/db'
+import { Bookmark } from '@/lib/db/schemas'
+
+export const DELETE: APIRoute = async (context) => {
+  // @TODO harden this!
+  // @TODO check batch or bulk deleteion options
+  const ids = await context.request.json()
+
+  const deletedBookmarkIds = ids.map(async (id: string) => {
+    const deletedBookmarkId: { deletedId: string }[] = await db.delete(Bookmark)
+      .where(eq(Bookmark.id, (id ? id : 'none')))
+      .returning({ deletedId: Bookmark.id})
+    
+      return deletedBookmarkId
+  })
+
+  return new Response(
+    JSON.stringify(deletedBookmarkIds), {
+      status: 200,
+    }
+  )
+}
