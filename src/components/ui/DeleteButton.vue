d5eeb6a24c7789a907b759ae59becee10edf5c27
Christian Bachhelm <cbachhelm@tuta.io>
2024-07-29 11:10:42 +0200

    Introduce delete-button component

---
+++ src/components/ui/DeleteButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Trash2 } from 'lucide-vue-next'
+
+const props = defineProps<{
+  buttonText: string
+}>()
+
+defineEmits<{
+  (e: 'delete'): void
+}>()
+</script>
+
+<template>
+  <Button
+    variant="destructive"
+    @click="$emit('delete')"
+  >
+    <Trash2 class="w-4 h-4 mr-2" />
+    {{ props.buttonText }}
+  </Button>
+</template>
