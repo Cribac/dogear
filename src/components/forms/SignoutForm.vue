74251b16486df8674ecc90ec476942fb28987c0e
Christian Bachhelm <cbachhelm@tuta.io>
2024-08-17 11:59:56 +0200

    Restructure signout button

--- src/components/forms/SignoutForm.vue
+++ src/components/forms/SignoutForm.vue
@@ -1,11 +1,17 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { LogOut } from 'lucide-vue-next'
+</script>
+
 <template>
   <form action="/api/auth/signout">
-    <Button type="submit">
+    <Button
+      size="xs"
+      class="bg-ctp-overlay1 hover:bg-ctp-surface2"
+      type="submit"
+    >
+      <LogOut class="mr-2 h-4 w-4" />
       Sign out
     </Button>
   </form>
 </template>
-
-<script setup lang="ts">
-import Button from '@/components/ui/button/Button.vue'
-</script>
