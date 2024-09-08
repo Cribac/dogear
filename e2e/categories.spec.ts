eb2c5101b89be99a09695cf5acb0329c9136e9d5
Christian Bachhelm <cbachhelm@tuta.io>
2024-09-08 21:03:05 +0200

    Use the beforEach hook in e2e tests to streamline testing

--- e2e/categories.spec.ts
+++ e2e/categories.spec.ts
@@ -1,21 +1,19 @@
 import { test, expect } from '@playwright/test'
 
-test('categories page has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/categories')
-  
+})
+
+test('categories page has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Categories/)
   await expect(page.getByRole('heading', { name: 'Categories' })).toBeVisible()
 })
 
 test ('categories page has avatar', async ({ page }) => {
-  await page.goto('/categories')
-
   await expect(page.getByText('AE')).toBeVisible()
 })
 
 test('categories page has create category form', async ({ page }) => {
-  await page.goto('/categories')
-
   await expect(page.getByLabel('Toggle Visibility')).toBeVisible()
   await page.getByLabel('Toggle Visibility').click()
   
@@ -24,8 +22,6 @@ test('categories page has create category form', async ({ page }) => {
 })
 
 test('categories page has data-table with correct columns', async ({ page }) => {
-  await page.goto('/categories')
-
   await expect(page.getByRole('cell', { name: 'Name' }).first()).toBeVisible()
   await expect(page.getByRole('cell', { name: '# of Bookmarks' }).first()).toBeVisible()
   await expect(page.locator('th').nth(3).first()).toBeVisible()
--- e2e/register-no-auth.spec.ts
+++ e2e/register-no-auth.spec.ts
@@ -3,9 +3,12 @@ import { test, expect } from '@playwright/test'
 // Reset storage state for this file to avoid being authenticated
 test.use({ storageState: { cookies: [], origins: [] } })
 
-test('register page shows register form', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/register')
-  
+  await page.waitForTimeout(500) // allow some time for the dom to render
+})
+
+test('register page shows register form', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Register Account' })).toBeVisible()
   await expect(page.getByLabel('Email')).toBeVisible()
   await expect(page.getByLabel('Password')).toBeVisible()
@@ -13,9 +16,6 @@ test('register page shows register form', async ({ page }) => {
 })
 
 test('it displays a message when email is invalid', async ({ page }) => {
-  await page.goto('/register')
-  await page.waitForTimeout(1000) // @TODO: needed rn to work in chromium; investigate why
-  
   await page.getByLabel('Email').click()
   await page.getByLabel('Email').fill('invalid')
   await page.getByRole('button', { name: 'Register' }).click()
@@ -24,21 +24,13 @@ test('it displays a message when email is invalid', async ({ page }) => {
 })
 
 test('it displays a message when no email is provided', async ({ page }) => {
-  await page.goto('/register')
-  await page.waitForTimeout(1000)
-
   await page.getByRole('button', { name: 'Register' }).click()
-  
   await expect(page.getByText('email is a required field')).toBeVisible()
 })
 
 test('it displays a message when no password is provided', async ({ page }) => {
-  await page.goto('/register')
-  await page.waitForTimeout(1000) // @TODO: needed rn to work in chromium; investigate why
-  
   await page.getByLabel('Email').click()
   await page.getByLabel('Email').fill('foo@example.com')
   await page.getByRole('button', { name: 'Register' }).click()
-  
   await expect(page.getByText('password is a required field')).toBeVisible()
 })
